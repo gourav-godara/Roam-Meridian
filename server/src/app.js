03e17637fbd7f084ac75caf6c00a110888501d73
gourav-godara <223130193+gourav-godara@users.noreply.github.com.>
2026-07-01 18:07:41 +0530

    Add environment variable template

---
+++ server/src/app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+// Import Routes
+const authRoutes = require("./routes/auth.routes");
+const userRoutes = require("./routes/user.routes");
+const destinationRoutes = require("./routes/destination.routes");
+const plannerRoutes = require("./routes/planner.routes");
+const reviewRoutes = require("./routes/review.routes");
+const expenseRoutes = require("./routes/expense.routes");
+
+const app = express();
+
+// ================= Middleware =================
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(cors());
+
+// ================= Routes =================
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/destinations", destinationRoutes);
+app.use("/api/planner", plannerRoutes);
+app.use("/api/reviews", reviewRoutes);
+app.use("/api/expenses", expenseRoutes);
+
+// Export App
+module.exports = app;
